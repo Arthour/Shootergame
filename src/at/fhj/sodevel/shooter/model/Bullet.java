3114e95bc089382226c0e2960863a78299af2427
Arthour <michael.thalhammer@outlook.de>
2017-05-26 01:39:17 +0200

    add placeholder rectangle

--- src/at/fhj/sodevel/shooter/model/Bullet.java
+++ src/at/fhj/sodevel/shooter/model/Bullet.java
@@ -5,6 +5,7 @@ import java.awt.*;
 public class Bullet extends NonControllableObjects {
     public Bullet(int x, int y) {
         position = new Point(x, y);
+        bounds = new Rectangle(x, y-11,24,13);
     }
 
     public Bullet(Point position) {
--- src/at/fhj/sodevel/shooter/model/Missile.java
+++ src/at/fhj/sodevel/shooter/model/Missile.java
@@ -5,6 +5,7 @@ import java.awt.*;
 public class Missile extends NonControllableObjects {
     public Missile(int x, int y) {
         position = new Point(x, y);
+        bounds = new Rectangle(x, y-11,24,13);
     }
 
     public Missile(Point position) {
